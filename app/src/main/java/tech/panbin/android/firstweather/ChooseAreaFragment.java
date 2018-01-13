82533386abaa2ff123e01739b63ec12d59aaf221
PanBinDev <bin.pan@foxmail.com>
2018-01-13 13:35:53 +0800

    添加"choose_area"界面

---
+++ app/src/main/java/tech/panbin/android/firstweather/ChooseAreaFragment.java
@@ -0,0 +1,11 @@
+package tech.panbin.android.firstweather;
+
+import android.app.Fragment;
+
+/**
+ * Created by PanBin on 2018/01/13.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+
+}
