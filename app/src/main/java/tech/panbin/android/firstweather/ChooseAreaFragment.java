f7e09a689b2ea52351427a65b89454c54add6a64
PanBinDev <bin.pan@foxmail.com>
2018-01-13 16:20:41 +0800

    finish "ChooseAreaFragment"

--- app/src/main/java/tech/panbin/android/firstweather/ChooseAreaFragment.java
+++ app/src/main/java/tech/panbin/android/firstweather/ChooseAreaFragment.java
@@ -12,13 +12,22 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 import tech.panbin.android.firstweather.db.City;
 import tech.panbin.android.firstweather.db.Country;
 import tech.panbin.android.firstweather.db.Province;
+import tech.panbin.android.firstweather.util.HttpUtil;
+import tech.panbin.android.firstweather.util.Utility;
 
 /**
  * Created by PanBin on 2018/01/13.
@@ -114,31 +123,142 @@ public class ChooseAreaFragment extends Fragment {
 
 //    查询所有的省，先查数据库，若没有再查服务器
     private void queryProvinces(){
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
 
+        provinceList = DataSupport.findAll(Province.class);
+
+        if(provinceList.size()>0){
+            dataList.clear();
+            for(Province province:provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+
+        }else{
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
     }
 
 //    查询所有的市，先查数据库，若没有再查服务器
     private void queryCities(){
 
+        titleText.setText(selectedProvince.getProvinceName());
+        backButton.setVisibility(View.VISIBLE);
+
+        cityList = DataSupport.where("provinceid = ?",String.valueOf(selectedProvince.getId())).find(City.class);
+        if(cityList.size()>0){
+            dataList.clear();
+            for(City city: cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_CITY;
+        }else{
+            int provinceCode = selectedProvince.getProvinceCode();
+            String address = "http://guolin.tech/api/china/" + provinceCode;
+        }
     }
 
 //    查询所有的县区，先查数据库，若没有再查服务器
     private void queryCountries(){
 
+        titleText.setText(selectedCity.getCityName());
+        backButton.setVisibility(View.VISIBLE);
+        countryList = DataSupport.where("cityid = ?",String.valueOf(selectedCity.getId())).find(Country.class);
+
+        if(countryList.size()>0){
+            dataList.clear();
+            for(Country country:countryList){
+                dataList.add(country.getCountryName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_COUNTRY;
+
+        }else{
+            int provinceCode = selectedProvince.getProvinceCode();
+            int cityCode = selectedCity.getCityCode();
+
+            String address = "http://guolin.tech/api/china/" + provinceCode + cityCode;
+            queryFromServer(address,"country");
+        }
     }
 
 //    从服务器查询
     private void queryFromServer(String address, final String type){
 
+        showProgressDialog();
+
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @TargetApi(Build.VERSION_CODES.M)
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+
+                if("province".equals(type)){
+                    result = Utility.handleProvinceResponse(responseText);
+                }
+                else if ("city".equals(type)){
+                    result = Utility.handleCityResponse(responseText, selectedProvince.getId());
+                }
+                else if ("country".equals(type)){
+                    result = Utility.handleCountryResponse(responseText, selectedCity.getId());
+                }
+
+                if(result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if("province".equals(type)){
+                                queryProvinces();
+                            }
+                            else if ("city".equals(type)){
+                                queryCities();
+                            }
+                            else if ("country".equals(type)){
+                                queryCountries();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
 
 //    显示进度对话框
     private void showProgressDialog(){
-
+        if(progressDialog == null){
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("正在加载...");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
     }
 
 //    关闭进度对话框
     private void closeProgressDialog(){
-
+        if(progressDialog != null){
+            progressDialog.dismiss();
+        }
     }
 }
