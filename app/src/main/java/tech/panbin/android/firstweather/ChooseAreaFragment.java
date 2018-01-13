856a1a201e2219e649fdc8ddaf0008fe236a0128
PanBinDev <bin.pan@foxmail.com>
2018-01-13 19:13:26 +0800

    fix crash bug

--- app/src/main/java/tech/panbin/android/firstweather/ChooseAreaFragment.java
+++ app/src/main/java/tech/panbin/android/firstweather/ChooseAreaFragment.java
@@ -1,12 +1,15 @@
 package tech.panbin.android.firstweather;
 
 import android.annotation.TargetApi;
-import android.app.Fragment;
+
 import android.app.ProgressDialog;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.*;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -74,10 +77,9 @@ public class ChooseAreaFragment extends Fragment {
 //    当前选中的级别
     private int currentLevel;
 
-    @TargetApi(Build.VERSION_CODES.M)
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.choose_area, container,false);
         titleText = (TextView) view.findViewById(R.id.title_text);
@@ -89,7 +91,7 @@ public class ChooseAreaFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
