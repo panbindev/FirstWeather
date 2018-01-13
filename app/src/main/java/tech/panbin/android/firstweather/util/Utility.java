d5a87be00b39354f09b223f7ee9f5aa3dacb4912
PanBinDev <bin.pan@foxmail.com>
2018-01-13 12:39:52 +0800

    解析省市区数据

---
+++ app/src/main/java/tech/panbin/android/firstweather/util/Utility.java
@@ -0,0 +1,90 @@
+package tech.panbin.android.firstweather.util;
+
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import tech.panbin.android.firstweather.db.City;
+import tech.panbin.android.firstweather.db.Country;
+import tech.panbin.android.firstweather.db.Province;
+
+/**
+ * Created by PanBin on 2018/01/13.
+ */
+
+public class Utility {
+
+//    解析返回的省级数据
+    public static boolean handleProvinceResponse(String response){
+
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0; i < allProvinces.length(); i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+
+                    Province province = new Province();
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.setProvinceName(provinceObject.getString("name"));
+
+                    province.save();
+                }
+                return true;
+
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+//    解析市级数据
+    public static boolean handleCityResponse(String response, int provinceId){
+        if(!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCites = new JSONArray(response);
+                for (int i = 0; i < allCites.length(); i++) {
+                    JSONObject cityObject = allCites.getJSONObject(i);
+
+                    City city = new City();
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setCityName(cityObject.getString("name"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+//    解析县区级数据
+    public static boolean handleCountryResponse(String response, int cityId){
+
+        if(!TextUtils.isEmpty(response)){
+
+            try {
+                JSONArray allCountries = new JSONArray(response);
+                for (int i = 0; i < allCountries.length(); i++) {
+                    JSONObject countryObject = allCountries.getJSONObject(i);
+
+                    Country country = new Country();
+                    country.setCountryName(countryObject.getString("name"));
+                    country.setWeatherId(countryObject.getString("weather_id"));
+                    country.setCityId(cityId);
+                    country.save();
+                }
+
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();;
+            }
+        }
+        return false;
+    }
+}
